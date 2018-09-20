c39b160f1a529bfe852961df0f49d7036383a291
piiingz <ingviun@stud.ntnu.no>
2018-09-20 19:20:02 +0200

    #19 Started styling the music button

--- src/components/playButton/PlayButton.js
+++ src/components/playButton/PlayButton.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "./PlayButton.css";
 
 const PlayButton = ({ src }) => {
   return (
-    <audio id="Music-player" controls autoPlay loop preload="auto">
+    <audio
+      id="Music-player"
+      className="PlayButton-audio"
+      controls
+      autoPlay
+      loop
+      preload="auto"
+    >
       <source src={src} />
       This text displays if the audio tag isn't supported.
     </audio>
